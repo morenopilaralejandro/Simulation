81ff85c95aec186bf08de97bb7cf03bb9c0a2ea5
alejandro <morenopilaralejandro@gmail.com>
2026-03-06 19:12:11 +0100

    feature(gameplay): world ui placeholder [#96]

---
+++ Assets/Scripts/UI/MenuSide.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using Simulation.Enums.Input;
+
+/*
+    menu things
+    MenuManager.Instance.ReplaceMenu(duelLogMenu);
+*/
+
+public class MenuSide : Menu
+{
+    private MenuManager menuManager;
+    private bool isOpen => menuManager.IsMenuOpen(this);
+    public bool IsSideMenuOpen => isOpen;
+
+    private void Awake()
+    {
+        //BattleUIManager.Instance.RegisterBattleMenu(this);
+    }
+
+    private void OnDestroy()
+    {
+        //BattleUIManager.Instance.UnregisterBattleMenu(this);
+    }
+
+    void Start()
+    {
+        menuManager = MenuManager.Instance;
+        base.Hide();
+        base.SetInteractable(false);
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (isOpen)
+        {
+            if (InputManager.Instance.GetDown(CustomAction.BattleUI_CloseBattleMenu))
+                Close();
+        } else 
+        {
+            if (InputManager.Instance.GetDown(CustomAction.BattleUI_OpenBattleMenu))
+                Open();
+        }
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        AudioManager.Instance.PlaySfx("sfx-menu_tap");
+    }
+
+    public override void Hide()
+    {
+        AudioManager.Instance.PlaySfx("sfx-menu_tap");
+        base.Hide();
+    }
+
+    public void Open()
+    {
+        if (menuManager.CurrentMenu != null && 
+            !menuManager.IsMenuOnTop(this)) 
+            return;
+
+        if (isOpen) return;
+
+        menuManager.OpenMenu(this);
+    }
+
+    public void Close()
+    {
+        if (!isOpen) return;
+        menuManager.CloseMenu();
+    }
+
+    public void OnButtonTapped()
+    {
+        //AutoBattleManager.Instance.ToggleAutoBattle();
+        Close();
+    }
+
+}
